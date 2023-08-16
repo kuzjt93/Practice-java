574a64c910400006df59787990272d608963980a
Long.H <longkr.work@gmail.com>
2023-08-16 18:41:47 -0500

    practice method with java

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package src;
+
+public class MultiplicationTable {
+    void print(int x) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d = %d", 5, x, x * 5).println();
+        }
+    }
+}
---
+++ src/MultiplicationTableRunner.java
@@ -0,0 +1,9 @@
+package src;
+
+public class MultiplicationTableRunner {
+    public static void main(String[] arges) {
+        MultiplicationTable table = new MultiplicationTable();
+
+        table.print(10);
+    }
+}
